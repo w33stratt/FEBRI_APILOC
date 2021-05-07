6a89373c3dbfcf8651afe800261c41ec14b7a076
Febri Febriansyah <80205709+w33stratt@users.noreply.github.com>
2021-05-07 23:48:47 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,21 @@
+const hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = hapi.server({
+        port: 5000,
+        host: 'localhost',
+        routes: {
+            cors: {
+                origin: ['*'],
+            },
+        },
+    });
+
+    server.route(routes);
+
+    await server.start();
+    console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+init();
